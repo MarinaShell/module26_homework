109d6821aa769903f8be5036a84315d550f094d3
Ponka Petrova <marinashell@mail.ru>
2022-12-21 13:01:55 +0300

    first commit for project

---
+++ src/README.md
@@ -0,0 +1,13 @@
+## ProjectForTesting
+## Description
+
+Написание тестов для задач.
+
+## Используемые технологии
+
+* JEST
+
+* Babel
+
+* JavaScript
+
---
+++ src/_tests_/index.test.js
@@ -0,0 +1,33 @@
+import {sum} from '../index.js'
+import {definePrime} from '../index.js'
+
+
+xdescribe("test sum", 
+()=>{
+    it("test sum1", 
+       ()=>{
+        expect(sum(1, 2)).toBe(3);
+       }
+    ),
+    it("test sum1", 
+       ()=>{
+        expect(sum(5, 100)).toBe(105);
+       }
+    );
+}
+);
+
+describe("tests for definePrime function", 
+    () => {
+        const simple = 17;
+        const complex = 18;
+    
+        it("should operate correctly with simple, complex and invalid number", 
+        () => {
+            expect(definePrime(simple)).toBe(`Число ${simple} - простое число`);
+            expect(definePrime(complex)).toBe(`Число ${complex} - составное число`);
+            expect(definePrime(1001)).toBe("Данные неверны");
+        }
+        );
+    }
+  );
